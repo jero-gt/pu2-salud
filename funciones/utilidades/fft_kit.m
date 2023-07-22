9d86064dbec91d95f822aea88b1aa6fe7e9ffce9
Ignacio Brittez <ignacio_brittez@outlook.com>
2023-07-22 11:22:41 -0300

    Creé la funcion fft_kit+

---
+++ funciones/utilidades/fft_kit.m
@@ -0,0 +1,14 @@
+function [X, X_shift, f] = fft_kit(x,fs)
+    X=fft(x);
+   
+    N=length(X);
+    % Si N es par
+    if(mod(N,2)==0)
+        f = (-fs/2 : (fs/N) : ((N-1)/N)*(fs/2));
+    else
+    % Si N es impar
+        f = (((N-1)/N)*(-fs/(2)) : (fs/N) : ((N-1)/N)*(fs/2));
+    end
+
+    X_shift = fftshift(X);
+end
