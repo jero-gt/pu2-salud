6b2c92aafdeea9ff3a51fb6be9ede90721b1d99d
jerogt <jeronimogomeztantarelli@yahoo.com.ar>
2023-07-31 20:49:59 -0300

    Desactivé el grid on en plotCompleto

--- funciones/utilidades/plotCompleto.m
+++ funciones/utilidades/plotCompleto.m
@@ -1,18 +1,19 @@
 function plotCompleto(lim_ejes,eje_x,eje_y,titulo,tam_letra,col,grosor,t,x)
     % plotCompleto(lim_ejes,eje_x,eje_y,titulo,tam_letra,col,grosor,t,x)
     %
-    % Realiza un gr ÃÅafico de una SVIC de manera adecuada.
+    % Realiza un gr Ã?afico de una SVIC de manera adecuada.
     % Ejemplo de uso:
     % t = -40:0.01:40;
     % xa = -2*sin(-0.2*t + 5/3*pi);
     % plotCompleto([t(1) t(end) -2 2],'t','f(t)','Se~nal f(t)',20,'r*-',1.5,t,xa)
     figure('units','normalized','outerposition',[0 0 1 1],'Name', titulo, 'NumberTitle', 'off'); % Creo y maximizo figura.
     plot(t,x,col,'Linewidth',grosor); % Grafico. Color (y marcador) y grosor.
-    axis(lim_ejes); grid on; % L ÃÅƒ±mites de los ejes. Grilla.
+    axis(lim_ejes); 
+    %grid on; % L Ã?ƒ±mites de los ejes. Grilla.
     set(gca,'FontSize', tam_letra); % Tama~no de letra para la leyenda y ejes.
     xlabel(eje_x,'Interpreter','Latex'); % Nombro el eje x.
     ylabel(eje_y,'Interpreter','Latex'); % Nombro el eje y.
-    title(titulo); % Coloco t ÃÅƒ±tulo para el gr ÃÅafico.
-    % Nota: Lo de 'interpreter', 'Latex' es opcional...s ÃÅolo modifica la
+    title(titulo); % Coloco t Ã?ƒ±tulo para el gr Ã?afico.
+    % Nota: Lo de 'interpreter', 'Latex' es opcional...s Ã?olo modifica la
     % fuente. Pueden googlear Latex como editor de textos...
 end
--- funciones/utilidades/stemCompleto.m
+++ funciones/utilidades/stemCompleto.m
@@ -2,7 +2,7 @@ function stemCompleto(lim_ejes,eje_x,eje_y,titulo,tam_letra,col,grosor,n,x)
 %stemCompleto([n(1) n(end) -1.5 1.5],'n','Amplitud','Se√±al de entrada x[n]',25,'m*-',1.5,n,h);
     % plotCompleto(lim_ejes,eje_x,eje_y,titulo,tam_letra,col,grosor,t,x)
     %
-    % Realiza un gr ÃÅafico de una SVIC de manera adecuada.
+    % Realiza un gr Ã?afico de una SVIC de manera adecuada.
     % Ejemplo de uso:
     % t = -40:0.01:40;
     % xa = -2*sin(-0.2*t + 5/3*pi);
@@ -10,10 +10,11 @@ function stemCompleto(lim_ejes,eje_x,eje_y,titulo,tam_letra,col,grosor,n,x)
     figure('units','normalized','outerposition',[0 0 1 1],'Name', titulo, 'NumberTitle', 'off'); % Creo y maximizo figura.
     stem(n, x, col, 'filled', 'Linewidth', grosor); % Grafico. Color (y marcador) y grosor.
     axis(lim_ejes);
+    %grid on
     set(gca,'FontSize', tam_letra); % Tama~no de letra para la leyenda y ejes.
     xlabel(eje_x,'Interpreter','Latex'); % Nombro el eje x.
     ylabel(eje_y,'Interpreter','Latex'); % Nombro el eje y.
-    title(titulo); % Coloco t ÃÅƒ±tulo para el gr ÃÅafico.
-    % Nota: Lo de 'interpreter', 'Latex' es opcional...s ÃÅolo modifica la
+    title(titulo); % Coloco t Ã?ƒ±tulo para el gr Ã?afico.
+    % Nota: Lo de 'interpreter', 'Latex' es opcional...s Ã?olo modifica la
     % fuente. Pueden googlear Latex como editor de textos...
 end
