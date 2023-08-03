0ee2a7b7e2bae01e1c09861d11e49ca8e8ff6d60
Ignacio Brittez <ignacio_brittez@outlook.com>
2023-08-03 08:42:01 -0300

    Funcion de plotstem arreglada

--- funciones/utilidades/plotStem.m
+++ funciones/utilidades/plotStem.m
@@ -1,4 +1,4 @@
-function plotStem(lim_ejes1,lim_ejes2, eje_x, eje_y, titulo1, titulo2, tam_letra, col, grosor, n1, x1, n2, x2)
+function plotStem(lim_ejes1,lim_ejes2, eje_x1, eje_y1, eje_x2, eje_y2, titulo1, titulo2, tam_letra, col, grosor, n1, x1, n2, x2)
     % stemCompleto(lim_ejes,eje_x,eje_y,titulo,tam_letra,col,grosor,t,x)
     %
     % Realiza un gr ?afico de una SVIC de manera adecuada.
@@ -12,8 +12,8 @@ function plotStem(lim_ejes1,lim_ejes2, eje_x, eje_y, titulo1, titulo2, tam_letra
     plot(n1, x1, col, 'Linewidth', grosor);
     axis(lim_ejes1); grid on; % L ??mites de los ejes. Grilla.
     view(0, 90); % Sitúa la gráfica en el eje real
-    xlabel(eje_x,'Interpreter','Latex'); % Nombro el eje x.
-    ylabel(eje_y,'Interpreter','Latex'); % Nombro el eje x.
+    xlabel(eje_x1,'Interpreter','Latex'); % Nombro el eje x.
+    ylabel(eje_y1,'Interpreter','Latex'); % Nombro el eje x.
     set(gca,'FontSize', tam_letra); % Tama~no de letra para la leyenda y ejes.
     title(titulo1); % Coloco t ??tulo para el gr ?afico.
     % Nota: Lo de 'interpreter', 'Latex' es opcional...s ?olo modifica la
@@ -23,8 +23,8 @@ function plotStem(lim_ejes1,lim_ejes2, eje_x, eje_y, titulo1, titulo2, tam_letra
     stem(n2, x2, col, 'Linewidth', grosor);
     axis(lim_ejes2); grid on; % L ??mites de los ejes. Grilla.
     view(0, 90); % Sitúa la gráfica en el eje real
-    xlabel(eje_x,'Interpreter','Latex'); % Nombro el eje x.
-    ylabel(eje_y,'Interpreter','Latex'); % Nombro el eje x.
+    xlabel(eje_x2,'Interpreter','Latex'); % Nombro el eje x.
+    ylabel(eje_y2,'Interpreter','Latex'); % Nombro el eje x.
     set(gca,'FontSize', tam_letra); % Tama~no de letra para la leyenda y ejes.
     title(titulo2); % Coloco t ??tulo para el gr ?afico.
 end
